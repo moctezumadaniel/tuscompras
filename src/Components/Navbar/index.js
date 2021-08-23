82945c815dcdc1eec405138dcb3e0b7a231d836a
moctezumadaniel <moctezumadanielt@gmail.com>
2021-08-23 16:42:01 -0500

    initial navbar component

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,14 +1,27 @@
-import MainMenu from "../MainMenu"
+import styles from './Navbar.module.css'
 export default function Navbar (){
     return(
-        <div>          
-            <button>
-            <i class="bi bi-list"></i>
-
+        <div className={styles.NavbarContainer}>          
+            <button className={styles.MainMenuButton}>
+                <i class="bi bi-list MainMenuIcon" 
+                style={{fontSize: '40px'}}>
+                </i>
             </button>
-            <form>
-                <input type='search'/>
-            </form>
+            <a className={styles.Logo}>
+                TusCompras
+            </a>
+           
+            <input type='search' 
+            className={styles.SearchBar}
+            placeholder="Buscar"/>
+            
+            <button className={styles.SellProductButton}>
+                Vender mi producto
+            </button>
+
+            <button className={styles.LogInAndOut}>
+                Iniciar sesion
+            </button>
         </div>
     )
 }
