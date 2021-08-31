1bc29e74074be9667833bf66b92904a50abb8efb
moctezumadaniel <moctezumadanielt@gmail.com>
2021-08-31 18:14:00 -0500

    logic to render product filters

--- src/Components/ProductFilters/index.js
+++ src/Components/ProductFilters/index.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { closeProductFilters, openProductFilters, toggleProductFilters } from '../../redux/actions/productFilters'
+import { toggleProductFilters } from '../../redux/actions/productFilters'
 import styles from './ProductFilters.module.css'
 const titles ={
     openFilters:'Filtrar Productos',
@@ -34,57 +34,65 @@ function ProductFilters (){
 
             <button className={styles.FilterButton}
             onClick={()=>openCloseFilters()}>
-            <i class="bi bi-funnel-fill"
-            style={{marginRight:"10px"}}></i>
-            {titles.openFilters}
+                <i class="bi bi-funnel-fill"
+                style={{marginRight:"10px"}}></i>
+                {titles.openFilters}
             </button>
 
-            <div>
-                {/* ORDER */}
-                <div className={styles.OrderContainer}>
-                    <div className={styles.FilterTitle}>
-                        {titles.orderBy}
-                    </div>
-                    <select className={styles.SelectInput}>
-                        <option>{titles.orderByCheaper}</option>
-                        <option>{titles.orderByMostExpensive}</option>
-                        <option>{titles.orderByBestRated}</option>
-                        <option>{titles.orderByMostImportant}</option>
-                    </select>
-                </div>
-                {/* PRICE */}
-                <div className={styles.PriceContainer}>
-                    <div className={styles.FilterTitle}>
-                        {titles.priceRange}
-                    </div>
-                    <div className={styles.AmounthInputContainer}>
-                        <div className={styles.AmounthContainer}>
-                            <div>{titles.priceFrom}</div>
-                            <input className={styles.AmounthInput}/>
-                        </div>
-                        <div className={styles.AmounthContainer}>
-                            <div>{titles.priceTo}</div>
-                            <input className={styles.AmounthInput}/>
-                        </div>
-                    </div>
-                </div>
-                {/* RATING */}
-                <div className={styles.RatingContainer}>
-                    <div className={styles.FilterTitle}>
-                        {titles.ratedWith}
-                    </div>
-                    <select className={styles.SelectInput}>
-                        <option>{titles.ratedTen}</option>
-                        <option>{titles.ratedNine}</option>
-                        <option>{titles.ratedEight}</option>
-                        <option>{titles.ratedSeven}</option>
-                        <option>{titles.ratedSix}</option>
-                        <option>{titles.everyRating}</option>
-                    </select>
-                </div>
-            </div>
+            <ListOfProducts filters={filters}/>
 
         </div>
     )
 }
 export default ProductFilters
+
+function ListOfProducts({filters}){
+    if(filters.status === 'opened')
+    return(
+        <div className={styles.FiltersContainer}>
+            {/* ORDER */}
+            <div className={styles.OrderContainer}>
+                <div className={styles.FilterTitle}>
+                    {titles.orderBy}
+                </div>
+                <select className={styles.SelectInput}>
+                    <option>{titles.orderByCheaper}</option>
+                    <option>{titles.orderByMostExpensive}</option>
+                    <option>{titles.orderByBestRated}</option>
+                    <option>{titles.orderByMostImportant}</option>
+                </select>
+            </div>
+            {/* PRICE */}
+            <div className={styles.PriceContainer}>
+                <div className={styles.FilterTitle}>
+                    {titles.priceRange}
+                </div>
+                <div className={styles.AmounthInputContainer}>
+                    <div className={styles.AmounthContainer}>
+                        <div>{titles.priceFrom}</div>
+                        <input className={styles.AmounthInput}/>
+                    </div>
+                    <div className={styles.AmounthContainer}>
+                        <div>{titles.priceTo}</div>
+                        <input className={styles.AmounthInput}/>
+                    </div>
+                </div>
+            </div>
+            {/* RATING */}
+            <div className={styles.RatingContainer}>
+                <div className={styles.FilterTitle}>
+                    {titles.ratedWith}
+                </div>
+                <select className={styles.SelectInput}>
+                    <option>{titles.ratedTen}</option>
+                    <option>{titles.ratedNine}</option>
+                    <option>{titles.ratedEight}</option>
+                    <option>{titles.ratedSeven}</option>
+                    <option>{titles.ratedSix}</option>
+                    <option>{titles.everyRating}</option>
+                </select>
+            </div>
+        </div>
+    )
+    else return ""
+}
