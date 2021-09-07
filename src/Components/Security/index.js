4c097d611a0e5266a51e659f82b2ef03d413754e
moctezumadaniel <moctezumadanielt@gmail.com>
2021-09-06 23:02:36 -0500

    initial security component

--- src/Components/Security/index.js
+++ src/Components/Security/index.js
@@ -1,7 +1,34 @@
 import styles from './Security.module.css'
+const titles = {
+    password:'Contraseña',
+    previousPassword:'Escribe tu contraseña anterior',
+    newPassword:'Escribe tu nueva contraseña',
+    confirmNewPassword:'Confirmar nueva contraseña',
+    changePassword:'Cambiar contraseña'
+}
 function Security (){
     return (
         <div>
+            <div className={styles.Titles}>
+                {titles.password}
+            </div>
+            <div className={styles.InputsContainer}>
+                <input 
+                placeholder={titles.previousPassword}
+                className={styles.Input}
+                type="password"/>
+                <input 
+                placeholder={titles.newPassword}
+                className={styles.Input}
+                type="password"/>
+                <input 
+                placeholder={titles.confirmNewPassword}
+                className={styles.Input}
+                type="password"/>
+                <button className={styles.PasswordButton}>
+                    {titles.changePassword}
+                </button>
+            </div>
 
         </div>
     )
