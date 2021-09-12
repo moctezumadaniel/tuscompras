1e5f43804b9e99974e3a055d9d586b1c2ade7db2
moctezumadaniel <moctezumadanielt@gmail.com>
2021-09-11 20:56:15 -0500

    initial customer returns filters and customer returns

--- src/Components/CustomersReturns/index.js
+++ src/Components/CustomersReturns/index.js
@@ -1,9 +1,33 @@
+import CustomersReturnsFilters from '../CustomersReturnsFilters'
+import ProductPreview from '../ProductPreview'
 import styles from './CustomersReturns.module.css'
 function CustomersReturns (){
     return (
         <div>
-
+            <CustomersReturnsFilters/>
+            <CustomersReturnsList/>
         </div>
     )
 }
 export default CustomersReturns
+
+function CustomersReturnsList(){
+    return(
+        <div className={styles.CustomersReturnsList}>
+            <ProductPreview 
+            title="ejemplo largo de la descripcion de un producto"
+            price="50000"
+            review="10/10"/>
+
+            <ProductPreview 
+            title="ejemplo largo de la descripcion de un producto"
+            price="50000"
+            review="10/10"/>
+
+            <ProductPreview 
+            title="ejemplo largo de la descripcion de un producto"
+            price="50000"
+            review="10/10"/>
+        </div>
+    )
+}
---
+++ src/Components/CustomersReturnsFilters/index.js
@@ -0,0 +1,105 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleReturnsFilters } from '../../redux/actions/returnsFilters'
+import styles from './CustomersReturnsFilters.module.css'
+const titles ={
+    openFilters:'Filtrar Devoluciones',
+    applyFilters:'Aplicar y cerrar filtros',
+    orderBy:'Ordenar por:',
+    totalRange:'Rango de totales:',
+    datesRange:'Rango de fechas',
+    ratedWith:'Calificados como',
+    // ORDER BY
+    latest:'Más recientes',
+    oldest:'Más antiguos',
+    greatestTotal:'Mayor total',
+    lowestTotal:'Menor total',
+    // PRICE
+    minTotal:'Desde',
+    maxTotal:'Hasta',
+    // CUSTOMER NAME
+    customerName:"Del cliente:",
+    customerNamePlaceholder:"Nombre del cliente a buscar"
+}
+function CustomersReturnsFilters (){
+    const filters = useSelector(state => state.ReturnsFilters)
+    const dispatch = useDispatch()
+    console.log(filters)
+    const openCloseFilters = () =>{
+        dispatch(toggleReturnsFilters())
+    }
+    return(
+        <div className={styles.FiltersContainer}>
+
+            <button className={styles.FilterButton}
+            onClick={()=>openCloseFilters()}>
+                <i class="bi bi-funnel-fill"
+                style={{marginRight:"10px"}}></i>
+                {filters.status === 'opened'?
+                titles.applyFilters
+                :titles.openFilters}
+            </button>
+
+            <ListOfFilters filters={filters}/>
+
+        </div>
+    )
+}
+export default CustomersReturnsFilters
+
+function ListOfFilters({filters}){
+    if(filters.status === 'opened')
+    return(
+        <div className={styles.ListOfFilters}>
+            {/* ORDER */}
+            <div className={styles.OrderContainer}>
+                <div className={styles.FilterTitle}>
+                    {titles.orderBy}
+                </div>
+                <select className={styles.SelectInput}>
+                    <option>{titles.latest}</option>
+                    <option>{titles.oldest}</option>
+                    <option>{titles.greatestTotal}</option>
+                    <option>{titles.lowestTotal}</option>
+                </select>
+            </div>
+            {/* TOTAL */}
+            <div className={styles.PriceContainer}>
+                <div className={styles.FilterTitle}>
+                    {titles.totalRange}
+                </div>
+                                       
+                        <input className={styles.AmounthInput}
+                        placeholder={titles.minTotal}
+                        type="number"/>
+                        <input className={styles.AmounthInput}
+                        placeholder={titles.maxTotal}
+                        type="number"/>
+               
+            </div>
+            {/* PERIOD */}
+            <div className={styles.PriceContainer}>
+                <div className={styles.FilterTitle}>
+                    {titles.datesRange}
+                </div>
+                                       
+                        <input className={styles.DatesInput}
+                        placeholder={titles.minTotal}
+                        type="date"/>
+                        <input className={styles.DatesInput}
+                        placeholder={titles.maxTotal}
+                        type="date"/>
+               
+            </div>
+            {/* CUSTOMER */}
+            <div className={styles.RatingContainer}>
+                <div className={styles.FilterTitle}>
+                    {titles.customerName}
+                </div>
+                <input type="search"
+                className={styles.SearchInput}
+                placeholder={titles.customerNamePlaceholder}/>
+            </div>
+        </div>
+    )
+    else return ""
+}
