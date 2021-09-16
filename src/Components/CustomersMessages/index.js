7b17b340fb22b06f9ceabc9583c828649700e147
moctezumadaniel <moctezumadanielt@gmail.com>
2021-09-16 18:32:57 -0500

    customer messages overflow error fixed

--- src/Components/CustomersMessages/index.js
+++ src/Components/CustomersMessages/index.js
@@ -2,11 +2,6 @@ import CustomerChat from '../CustomerChat'
 import CustomersListOfChats from '../CustomersListOfChats'
 import styles from './CustomersMessages.module.css'
 function CustomersMessages (){
-    const main = document.getElementById('Main')
-    function hidOverflow(){
-        main.style.overflow = "hidden"
-    }
-    hidOverflow()
     return (
         <div className={styles.CustomersMessages}>
             <CustomersListOfChats/>
