275366f99b66aaa6e295d59d1ad0a8646f746f54
moctezumadaniel <moctezumadanielt@gmail.com>
2021-09-18 21:36:48 -0500

    initial product settings images components

--- src/Components/ProductSettings/index.js
+++ src/Components/ProductSettings/index.js
@@ -21,16 +21,7 @@ function ProductSettings(){
                 className={styles.ShortDescription}/>
             </div>
 
-            <div>
-                <div className={styles.MainTitle}>
-                    {titles.images}
-                </div>
-                <div className={styles.AddImageContainer}>
-                    <button className={styles.AddImage}>
-                        {titles.addImage}
-                    </button>
-                </div>
-            </div>
+            <ProductImages/>
 
             <div className={styles.DescriptionsContainer}>
                 <textarea placeholder={titles.completeDescription}
@@ -41,3 +32,43 @@ function ProductSettings(){
     )
 }
 export default ProductSettings
+
+function ProductImages(){
+    return (
+        <div>
+            <div className={styles.MainTitle}>
+                {titles.images}
+            </div>
+            <div className={styles.ListOfImages}>
+                <div className={styles.ImageContainer}>
+                    <div className={styles.Image}>
+                        <img/>
+                    </div>
+                    
+                    <div className={styles.ImageButtons}>
+                        <button className={styles.DeleteImage}>
+                            {titles.delete}
+                        </button>
+                        <button className={styles.ChangeImage}>
+                            {titles.change}
+                        </button>
+                    </div>
+                </div>
+
+                <div className={styles.ImageContainer}>
+                    <div className={styles.Image}>
+                        <img/>
+                    </div>
+                    
+                    <div className={styles.ImageButtons}>
+                        <button className={styles.AddImage}>
+                            {titles.addImage}
+                        </button>
+                    </div>
+                </div>
+            </div>
+            
+            
+        </div>
+    )
+}
