acba799084ff4a44b97ce3ce91149ab2c8e67238
moctezumadaniel <moctezumadanielt@gmail.com>
2021-09-24 20:38:54 -0500

    initial set up for mongodb

---
+++ models/example.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+const customerInformationSchema = mongoose.Schema({
+    //ORDERS TOOL
+    customerId:String,
+    orders:[{
+        number:String,
+        date:Date,
+        description:String,
+        fullfiled:Boolean,
+        items:[{
+            id:String,
+            description:String,
+            amounth:Number,
+        }]
+    }],
+
+    //TICKETS TOOL
+    tickets:[{
+        number:String,
+        date:Date,
+        name:String,
+        items:[{
+            id:String,
+            description:String,
+            amounth:Number,
+        }]
+    }],
+
+    //EXPENSES TOOL
+    expenses:[{
+        date:Date,
+        amounth:Number,
+        description:String
+    }],
+
+    //CUSTOMERS TOOL
+    customers:[{
+        name:String,
+        date:Date,
+        amounth:Number,
+        description:String,
+        operation:String,
+    }],
+    //PROVIDERS TOOL
+    providers:[{
+        name:String,
+        date:Date,
+        amounth:Number,
+        description:String,
+        operation:String,
+    }]
+})
+
+const CustomerInformation = mongoose.models.CustomerInformation 
+    || mongoose.model('CustomerInformation',customerInformationSchema)
+module.exports = CustomerInformation
