c80de7c5f8c0b9b39e33d1137d191023d7ee262d
moctezumadaniel <moctezumadanielt@gmail.com>
2021-10-02 20:57:23 -0500

    main comparision and switch breaks

--- src/redux/reducers/CustomerReturnsFilters.js
+++ src/redux/reducers/CustomerReturnsFilters.js
@@ -1,39 +1,41 @@
-const initialState ={
-    status:'',
-    orderBy:'',
-    minTotal:'',
-    maxTotal:'',
-    initialDate:'',
-    finalDate:'',
-    customerName:'',
-
-}
-export const CustomersReturnsFiltersReducer = (state=initialState, action) =>{
-    switch(action.type){
-        case 'OPEN_CUSTOMERS_RETURNS_FILTERS':
-            return{
-                ...state,
-                status:'opened'
-            }
-        case 'CLOSE_CUSTOMERS_RETURNS_FILTERS':
-            return{
-                ...state,
-                status:'closed'
-            }
-        case 'TOGGLE_CUSTOMERS_RETURNS_FILTERS':
-            if(!state.status || state.status == 'closed'){
-                return{
-                    ...state,
-                    status:'opened'
-                }
-            }
-            else if(state.status == 'opened'){
-                return{
-                    ...state,
-                    status:'closed'
-                }
-            }
-        default:
-            return state
-    }
-}
+const initialState = {
+  status: "",
+  orderBy: "",
+  minTotal: "",
+  maxTotal: "",
+  initialDate: "",
+  finalDate: "",
+  customerName: "",
+};
+export const CustomersReturnsFiltersReducer = (
+  state = initialState,
+  action
+) => {
+  switch (action.type) {
+    case "OPEN_CUSTOMERS_RETURNS_FILTERS":
+      return {
+        ...state,
+        status: "opened",
+      };
+    case "CLOSE_CUSTOMERS_RETURNS_FILTERS":
+      return {
+        ...state,
+        status: "closed",
+      };
+    case "TOGGLE_CUSTOMERS_RETURNS_FILTERS":
+      if (!state.status || state.status === "closed") {
+        return {
+          ...state,
+          status: "opened",
+        };
+      } else if (state.status === "opened") {
+        return {
+          ...state,
+          status: "closed",
+        };
+      }
+      break;
+    default:
+      return state;
+  }
+};
--- src/redux/reducers/ProductFilters.js
+++ src/redux/reducers/ProductFilters.js
@@ -1,37 +1,36 @@
-const initialState ={
-    status:'',
-    orderBy:'',
-    priceFrom:'',
-    priceTo:'',
-    ratedWith:''
-
-}
-export const ProductFiltersReducer = (state=initialState, action) =>{
-    switch(action.type){
-        case 'OPEN_PRODUCT_FILTERS':
-            return{
-                ...state,
-                status:'opened'
-            }
-        case 'CLOSE_PRODUCT_FILTERS':
-            return{
-                ...state,
-                status:'closed'
-            }
-        case 'TOGGLE_PRODUCT_FILTERS':
-            if(!state.status || state.status == 'closed'){
-                return{
-                    ...state,
-                    status:'opened'
-                }
-            }
-            else if(state.status == 'opened'){
-                return{
-                    ...state,
-                    status:'closed'
-                }
-            }
-        default:
-            return state
-    }
-}
+const initialState = {
+  status: "",
+  orderBy: "",
+  priceFrom: "",
+  priceTo: "",
+  ratedWith: "",
+};
+export const ProductFiltersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_PRODUCT_FILTERS":
+      return {
+        ...state,
+        status: "opened",
+      };
+    case "CLOSE_PRODUCT_FILTERS":
+      return {
+        ...state,
+        status: "closed",
+      };
+    case "TOGGLE_PRODUCT_FILTERS":
+      if (!state.status || state.status === "closed") {
+        return {
+          ...state,
+          status: "opened",
+        };
+      } else if (state.status === "opened") {
+        return {
+          ...state,
+          status: "closed",
+        };
+      }
+      break;
+    default:
+      return state;
+  }
+};
