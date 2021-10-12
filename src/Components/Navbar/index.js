d4dd738535ff5561733426219f82d6a0aaf3999d
moctezumadaniel <moctezumadanielt@gmail.com>
2021-10-11 19:21:48 -0500

    roboto font for buttons and overflow show when the search is pressed

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,82 +1,82 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { setMainMenuToClose, setMainMenuToOpen } from '../../redux/actions/mainMenu'
-import { activateProductSearch, deactivateProductSearch } from '../../redux/actions/navbar'
-import styles from './Navbar.module.css'
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setMainMenuToClose,
+  setMainMenuToOpen,
+} from "../../redux/actions/mainMenu";
+import {
+  activateProductSearch,
+  deactivateProductSearch,
+} from "../../redux/actions/navbar";
+import styles from "./Navbar.module.css";
 const titles = {
-    sell:'Vender mi producto',
-    login: 'Iniciar sesion'
+  sell: "Vender mi producto",
+  login: "Iniciar sesion",
+};
+
+export default function Navbar() {
+  const navbarState = useSelector((state) => state.Navbar);
+  const searchState = navbarState.activeSearch;
+  const menuStatus = useSelector((state) => state.MainMenu.status);
+  const dispatch = useDispatch();
+  function activateSearch() {
+    if (menuStatus === "opened") {
+      dispatch(setMainMenuToClose());
+      document.getElementById("body").style.overflow = "visible";
+    }
+    dispatch(activateProductSearch());
+  }
+  function deactivateSearch() {
+    dispatch(deactivateProductSearch());
+  }
+  return (
+    <div className={styles.NavbarContainer}>
+      <MenuButton />
+
+      <a className={searchState ? styles.LogoSA : styles.Logo} href="/">
+        TusCompras
+      </a>
+
+      <input
+        type="search"
+        onFocus={() => activateSearch()}
+        onBlur={() => deactivateSearch()}
+        className={searchState ? styles.SearchBarSA : styles.SearchBar}
+        placeholder="Buscar"
+      />
+
+      <a className={styles.SellProductButton} href="vender">
+        {titles.sell}
+      </a>
+
+      <a className={styles.LogInAndOut} href="iniciar-sesion">
+        {titles.login}
+      </a>
+    </div>
+  );
 }
 
-export default function Navbar (){
-    const navbarState = useSelector(state => state.Navbar)
-    const searchState = navbarState.activeSearch
-    const menuStatus = useSelector(state => state.MainMenu.status)
-    const dispatch = useDispatch()
-    function activateSearch(){
-        
-        if(menuStatus === 'opened'){
-            dispatch(setMainMenuToClose())
-        }
-        dispatch(activateProductSearch())
-    }
-    function deactivateSearch(){
-        dispatch(deactivateProductSearch())
-    }
-    return(
-        <div className={styles.NavbarContainer}>          
-            <MenuButton/>
-
-            <a className={searchState ? styles.LogoSA: styles.Logo} 
-            href="/">
-                TusCompras
-            </a>
-           
-            <input type='search' 
-            onFocus={()=>activateSearch()}
-            onBlur={()=>deactivateSearch()}
-            className={searchState ?  styles.SearchBarSA: styles.SearchBar}
-            placeholder="Buscar"/>
-            
-       
-            <a className={styles.SellProductButton} href="vender">
-            {titles.sell}
-            </a>
-            
-
-            <a className={styles.LogInAndOut} href="iniciar-sesion">
-                {titles.login}
-            </a>
-        </div>
-    )
-}
-
-function MenuButton(){
-    const menuStatus = useSelector(state => state.MainMenu.status)
-    const body = document.getElementById('body')
-    const dispatch = useDispatch()
-    const openMainMenu = () =>{
-        dispatch(setMainMenuToOpen())
-        body.style.overflow = 'hidden'
-    }
-    const closeMainMenu = () =>{
-        dispatch(setMainMenuToClose())
-        body.style.overflow = 'visible'
-    }
-    if(menuStatus === 'opened'){
-        return(
-        <button className={styles.MainMenuButton}
-        onClick={()=>closeMainMenu()}>
-            <i class="bi bi-x" 
-            style={{fontSize: '40px'}}>
-            </i>
-        </button>
-        )
-    }else return(
-        <button className={styles.MainMenuButton}
-        onClick={()=>openMainMenu()}>
-            <i class="bi bi-list MainMenuIcon" 
-            style={{fontSize: '40px'}}>
-            </i>
-        </button>
-    )
+function MenuButton() {
+  const menuStatus = useSelector((state) => state.MainMenu.status);
+  const body = document.getElementById("body");
+  const dispatch = useDispatch();
+  const openMainMenu = () => {
+    dispatch(setMainMenuToOpen());
+    body.style.overflow = "hidden";
+  };
+  const closeMainMenu = () => {
+    dispatch(setMainMenuToClose());
+    body.style.overflow = "visible";
+  };
+  if (menuStatus === "opened") {
+    return (
+      <button className={styles.MainMenuButton} onClick={() => closeMainMenu()}>
+        <i class="bi bi-x" style={{ fontSize: "40px" }}></i>
+      </button>
+    );
+  } else
+    return (
+      <button className={styles.MainMenuButton} onClick={() => openMainMenu()}>
+        <i class="bi bi-list MainMenuIcon" style={{ fontSize: "40px" }}></i>
+      </button>
+    );
 }
