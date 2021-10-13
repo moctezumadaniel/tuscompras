57439bfa7b14f33142d94352c179c786b39c1b46
moctezumadaniel <moctezumadanielt@gmail.com>
2021-10-12 19:50:11 -0500

    chat separator style normilized

--- src/Components/CustomersListOfChats/index.js
+++ src/Components/CustomersListOfChats/index.js
@@ -1,113 +1,113 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { hideCustomersListOfChats } from '../../redux/actions/customerChats'
-import styles from './CustomersListOfChats.module.css'
+import { useDispatch, useSelector } from "react-redux";
+import { hideCustomersListOfChats } from "../../redux/actions/customerChats";
+import styles from "./CustomersListOfChats.module.css";
 const titles = {
-    chatSearch:'Buscar nombre del cliente'
-}
-function CustomersListOfChats(){
-    const customerChats = useSelector(state => state.CustomerChats)
-    
-    return(
-        <div className={customerChats.listOfChats ? styles.MainContainer: styles.MainContainerHided}>
-            <input type="search"
-            className={styles.ChatSearch}
-            placeholder={titles.chatSearch}/>
-            <ListOfChats/>
+  chatSearch: "Buscar nombre del cliente",
+};
+function CustomersListOfChats() {
+  const customerChats = useSelector((state) => state.CustomerChats);
 
-        </div>
-    )
-  
+  return (
+    <div
+      className={
+        customerChats.listOfChats
+          ? styles.MainContainer
+          : styles.MainContainerHided
+      }
+    >
+      <input
+        type="search"
+        className={styles.ChatSearch}
+        placeholder={titles.chatSearch}
+      />
+      <ListOfChats />
+    </div>
+  );
 }
-export default CustomersListOfChats
+export default CustomersListOfChats;
 
-function ListOfChats(){
-    return(
-        <div className={styles.ListOfChats}>
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            
-        </div>
-    )
+function ListOfChats() {
+  return (
+    <div className={styles.ListOfChats}>
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+    </div>
+  );
 }
 
-function ChatPreview({name, lastMessage}){
-    const dispatch = useDispatch()
-    function selectChat(){
-        if(window.innerWidth < 750)
-        dispatch(hideCustomersListOfChats())
-    }
-    return(
-        <div onClick={()=>selectChat()}>
-            <div className={styles.ChatPreview}>
-            <img width="50" height="50"/>
-                <div className={styles.ChatDescription}>
-                    <div className={styles.ChatName}>
-                        {name}
-                    </div>
-                    <div>
-                        {lastMessage}
-                    </div>
-                </div>
-            </div>
-            <hr/>
+function ChatPreview({ name, lastMessage }) {
+  const dispatch = useDispatch();
+  function selectChat() {
+    if (window.innerWidth < 750) dispatch(hideCustomersListOfChats());
+  }
+  return (
+    <div onClick={() => selectChat()}>
+      <div className={styles.ChatPreview}>
+        <img width="50" height="50" />
+        <div className={styles.ChatDescription}>
+          <div className={styles.ChatName}>{name}</div>
+          <div>{lastMessage}</div>
         </div>
-    )
+      </div>
+      <hr className={styles.ChatSeparator} />
+    </div>
+  );
 }
--- src/Components/SellersListOfChats/index.js
+++ src/Components/SellersListOfChats/index.js
@@ -1,111 +1,112 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { hideSellersListOfChats } from '../../redux/actions/sellerChats'
-import styles from './SellersListOfChats.module.css'
+import { useDispatch, useSelector } from "react-redux";
+import { hideSellersListOfChats } from "../../redux/actions/sellerChats";
+import styles from "./SellersListOfChats.module.css";
 const titles = {
-    chatSearch:'Buscar nombre del vendedor'
+  chatSearch: "Buscar nombre del vendedor",
+};
+function SellersListOfChats() {
+  const sellerChats = useSelector((state) => state.SellerChats);
+  return (
+    <div
+      className={
+        sellerChats.listOfChats
+          ? styles.MainContainer
+          : styles.MainContainerHided
+      }
+    >
+      <input
+        type="search"
+        className={styles.ChatSearch}
+        placeholder={titles.chatSearch}
+      />
+      <ListOfChats />
+    </div>
+  );
 }
-function SellersListOfChats(){
-    const sellerChats = useSelector(state => state.SellerChats)
-    return(
-        <div className={sellerChats.listOfChats ?  styles.MainContainer : styles.MainContainerHided}>
-            <input type="search"
-            className={styles.ChatSearch}
-            placeholder={titles.chatSearch}/>
-            <ListOfChats/>
+export default SellersListOfChats;
 
-        </div>
-    )
-}
-export default SellersListOfChats
-
-function ListOfChats(){
-    return(
-        <div className={styles.ListOfChats}>
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            <ChatPreview
-            name={"Juan Daniel Trejo Moctezuma"}
-            lastMessage={"Quiero hacer una pregunta sobre"}
-            />
-            
-        </div>
-    )
+function ListOfChats() {
+  return (
+    <div className={styles.ListOfChats}>
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+      <ChatPreview
+        name={"Juan Daniel Trejo Moctezuma"}
+        lastMessage={"Quiero hacer una pregunta sobre"}
+      />
+    </div>
+  );
 }
 
-function ChatPreview({name, lastMessage}){
-    const dispatch = useDispatch()
-    function selectChat(){
-        if(window.innerWidth < 750)
-        dispatch(hideSellersListOfChats())
-    }
-    return(
-        <div onClick={()=>selectChat()}>
-            <div className={styles.ChatPreview}>
-            <img width="50" height="50"/>
-                <div className={styles.ChatDescription}>
-                    <div className={styles.ChatName}>
-                        {name}
-                    </div>
-                    <div>
-                        {lastMessage}
-                    </div>
-                </div>
-            </div>
-            <hr/>
+function ChatPreview({ name, lastMessage }) {
+  const dispatch = useDispatch();
+  function selectChat() {
+    if (window.innerWidth < 750) dispatch(hideSellersListOfChats());
+  }
+  return (
+    <div onClick={() => selectChat()}>
+      <div className={styles.ChatPreview}>
+        <img width="50" height="50" />
+        <div className={styles.ChatDescription}>
+          <div className={styles.ChatName}>{name}</div>
+          <div>{lastMessage}</div>
         </div>
-    )
+      </div>
+      <hr className={styles.ChatSeparator} />
+    </div>
+  );
 }
