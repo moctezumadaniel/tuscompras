59b7ea0d40db78d76246e9aae823c6537e1bb9a2
moctezumadaniel <moctezumadanielt@gmail.com>
2021-10-14 18:35:32 -0500

    main componenets splited to reduce bundle

--- src/Components/MainScreen/index.js
+++ src/Components/MainScreen/index.js
@@ -1,85 +1,81 @@
-import styles from './MainScreen.module.css'
-import ListOfProducts from "../ListOfProducts"
-import MainMenu from "../MainMenu"
-import Navbar from "../Navbar"
-import { Switch , BrowserRouter as Router, Route} from 'react-router-dom'
-import UserProducts from '../UserProducts'
-import CustomersReturns from '../CustomersReturns'
-import CustomersMessages from '../CustomersMessages'
-import Purchases from '../Purchases'
-import Returns from '../Returns'
-import SellersMessages from '../SellersMessages'
-import Profile from '../Profile'
-import Security from '../Security'
-import ProductSettings from '../ProductSettings'
-import Sales from '../Sales'
-import ProductPage from '../ProductPage'
+import styles from "./MainScreen.module.css";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import Navbar from "../Navbar";
+import MainMenu from "../MainMenu";
 
-export default function MainScreen (){
-    return(
-        <Router>
-        <div className={styles.MainScreenContainer} id='Main'>
-            <Navbar/>
-            <MainMenu/>
-            
-                 <Switch>
+const ListOfProducts = React.lazy(() => import("../ListOfProducts"));
+const UserProducts = React.lazy(() => import("../UserProducts"));
+const CustomersReturns = React.lazy(() => import("../CustomersReturns"));
+const CustomersMessages = React.lazy(() => import("../CustomersMessages"));
+const Purchases = React.lazy(() => import("../Purchases"));
+const Returns = React.lazy(() => import("../Returns"));
+const SellersMessages = React.lazy(() => import("../SellersMessages"));
+const Profile = React.lazy(() => import("../Profile"));
+const Security = React.lazy(() => import("../Security"));
+const ProductSettings = React.lazy(() => import("../ProductSettings"));
+const Sales = React.lazy(() => import("../Sales"));
+const ProductPage = React.lazy(() => import("../ProductPage"));
 
-                    
-                    
-                    <Route path="/productos">
-                        <UserProducts/>
-                    </Route>
-                        
-                    <Route path="/devoluciones-de-clientes">
-                        <CustomersReturns/>
-                    </Route>
-                        
-                    <Route path="/mensajes-de-clientes">
-                        <CustomersMessages/>
-                    </Route>
+export default function MainScreen() {
+  return (
+    <React.Suspense fallback={<span>Cargando...</span>}>
+      <Router>
+        <div className={styles.MainScreenContainer} id="Main">
+          <Navbar />
+          <MainMenu />
 
-                    <Route path="/compras">
-                        <Purchases/>
-                    </Route>
+          <Switch>
+            <Route path="/productos">
+              <UserProducts />
+            </Route>
 
-                    <Route path="/devoluciones">
-                        <Returns/>
-                    </Route>
+            <Route path="/devoluciones-de-clientes">
+              <CustomersReturns />
+            </Route>
 
-                    <Route path="/mensajes-de-vendedores">
-                        <SellersMessages/>
-                    </Route>
+            <Route path="/mensajes-de-clientes">
+              <CustomersMessages />
+            </Route>
 
-                    <Route path="/perfil">
-                        <Profile/>
-                    </Route>
+            <Route path="/compras">
+              <Purchases />
+            </Route>
 
-                    <Route path="/seguridad">
-                        <Security/>
-                    </Route>
-                    
-                    <Route path="/ventas">
-                        <Sales/>
-                    </Route>
+            <Route path="/devoluciones">
+              <Returns />
+            </Route>
 
-                    <Route path="/vender">
-                        <ProductSettings/>
-                    </Route>
+            <Route path="/mensajes-de-vendedores">
+              <SellersMessages />
+            </Route>
 
-                    <Route path="/producto">
-                        <ProductPage/>
-                    </Route>
+            <Route path="/perfil">
+              <Profile />
+            </Route>
 
-                    <Route path="/">
-                        <ListOfProducts/>
-                    </Route>
-                    
-                </Switch>
-            
-            
+            <Route path="/seguridad">
+              <Security />
+            </Route>
 
-            
+            <Route path="/ventas">
+              <Sales />
+            </Route>
+
+            <Route path="/vender">
+              <ProductSettings />
+            </Route>
+
+            <Route path="/producto">
+              <ProductPage />
+            </Route>
+
+            <Route path="/">
+              <ListOfProducts />
+            </Route>
+          </Switch>
         </div>
-        </Router>
-    )
+      </Router>
+    </React.Suspense>
+  );
 }
