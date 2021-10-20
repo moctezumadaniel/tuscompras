0a058b00a3ec346d523130cdc6199a2825221fa2
moctezumadaniel <moctezumadanielt@gmail.com>
2021-10-19 20:03:30 -0500

    pwa initial configuration added

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import rootReducer from './redux/reducers';
-import { Provider } from 'react-redux';
+import rootReducer from "./redux/reducers";
+import { Provider } from "react-redux";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 ReactDOM.render(
   <React.StrictMode>
-
     <Provider store={store}>
       <App />
     </Provider>
-    
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
