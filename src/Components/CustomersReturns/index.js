dc663789a14731aac1fdead15f15e7b403f3dfd4
moctezumadaniel <moctezumadanielt@gmail.com>
2022-03-07 19:40:03 -0600

    main menu is authenticated logic, duplications deleted and initial loading menu component added

--- src/Components/CustomersReturns/index.js
+++ src/Components/CustomersReturns/index.js
@@ -1,33 +1,36 @@
-import CustomersReturnsFilters from '../CustomersReturnsFilters'
-import ProductPreview from '../ProductPreview'
-import styles from './CustomersReturns.module.css'
-function CustomersReturns (){
-    return (
-        <div>
-            <CustomersReturnsFilters/>
-            <CustomersReturnsList/>
-        </div>
-    )
+import CustomersReturnsFilters from "../CustomersReturnsFilters";
+import ProductPreview from "../ProductPreview";
+import styles from "./CustomersReturns.module.css";
+function CustomersReturns() {
+  return (
+    <div>
+      <CustomersReturnsFilters />
+      <CustomersReturnsList />
+    </div>
+  );
 }
-export default CustomersReturns
+export default CustomersReturns;
 
-function CustomersReturnsList(){
-    return(
-        <div className={styles.CustomersReturnsList}>
-            <ProductPreview 
-            title="ejemplo largo de la descripcion de un producto"
-            price="50000"
-            review="10/10"/>
+function CustomersReturnsList() {
+  return (
+    <div className={styles.CustomersReturnsList}>
+      <ProductPreview
+        title="ejemplo largo de la descripcion de un producto"
+        price="50000"
+        review="10/10"
+      />
 
-            <ProductPreview 
-            title="ejemplo largo de la descripcion de un producto"
-            price="50000"
-            review="10/10"/>
+      <ProductPreview
+        title="ejemplo largo de la descripcion de un producto"
+        price="50000"
+        review="10/10"
+      />
 
-            <ProductPreview 
-            title="ejemplo largo de la descripcion de un producto"
-            price="50000"
-            review="10/10"/>
-        </div>
-    )
+      <ProductPreview
+        title="ejemplo largo de la descripcion de un producto"
+        price="50000"
+        review="10/10"
+      />
+    </div>
+  );
 }
--- src/Components/MainMenu/index.js
+++ src/Components/MainMenu/index.js
@@ -29,9 +29,55 @@ const titles = {
 export default function MainMenu() {
   const menuStatus = useSelector((state) => state.MainMenu.status);
   const { isLoading, isAuthenticated } = useAuth0();
+  const body = document.getElementById("body");
+  const currentUrl = window.location.pathname;
   const loggedIn = isAuthenticated && !isLoading ? true : false;
+  const dispatch = useDispatch();
+  const closeMainMenu = () => {
+    dispatch(setMainMenuToClose());
+    if (
+      currentUrl !== "/mensajes-de-vendedores" &&
+      currentUrl !== "/mensajes-de-clientes"
+    )
+      body.style.overflow = "visible";
+  };
   if (menuStatus === "opened")
-    return <>{loggedIn ? <LoggedInMenu /> : <LoggedOutMenu />}</>;
+    return (
+      <Router>
+        <div
+          className={
+            menuStatus === "opened"
+              ? styles.menuContainer
+              : styles.menuContainerHided
+          }
+        >
+          <ul
+            className={
+              menuStatus === "opened"
+                ? styles.menuCanvas
+                : styles.menuCanvasHided
+            }
+          >
+            {loggedIn ? (
+              <LoggedInMenu />
+            ) : !isAuthenticated && !isLoading ? (
+              <LoggedOutMenu />
+            ) : (
+              <LoadingLoginMenu />
+            )}
+          </ul>
+
+          <div
+            className={
+              menuStatus === "opened"
+                ? styles.menuBackground
+                : styles.menuBackgroundHided
+            }
+            onClick={() => closeMainMenu()}
+          ></div>
+        </div>
+      </Router>
+    );
   else return "";
 }
 
@@ -51,183 +97,137 @@ const LoggedInMenu = () => {
   };
   console.log(menuStatus);
   return (
-    <Router>
-      <div
-        className={
-          menuStatus === "opened"
-            ? styles.menuContainer
-            : styles.menuContainerHided
-        }
-      >
-        <ul
-          className={
-            menuStatus === "opened" ? styles.menuCanvas : styles.menuCanvasHided
-          }
-        >
-          <a className={styles.SellProductButton} href="/vender">
-            {titles.sell}
-          </a>
+    <>
+      <a className={styles.SellProductButton} href="/vender">
+        {titles.sell}
+      </a>
 
-          <li>
-            <h3>{titles.sales}</h3>
-          </li>
+      <li>
+        <h3>{titles.sales}</h3>
+      </li>
 
-          <li>
-            <a
-              className={styles.link}
-              href="/productos"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.products}
-            </a>
-          </li>
-          <li>
-            <a
-              className={styles.link}
-              href="/ventas"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.salesList}
-            </a>
-          </li>
-          <li>
-            <a
-              className={styles.link}
-              href="/devoluciones-de-clientes"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.buyersReturns}
-            </a>
-          </li>
-          <li>
-            <a
-              className={styles.link}
-              href="/mensajes-de-clientes"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.buyersMessages}
-            </a>
-          </li>
-
-          <li>
-            <h3>{titles.purchases}</h3>
-          </li>
-
-          <li>
-            <a
-              className={styles.link}
-              href="/compras"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.purchasesList}
-            </a>
-          </li>
-          <li>
-            <a
-              className={styles.link}
-              href="/devoluciones"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.returns}
-            </a>
-          </li>
-          <li>
-            <a
-              className={styles.link}
-              href="/mensajes-de-vendedores"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.sellersMessages}
-            </a>
-          </li>
-          <li>
-            <h3>{titles.profile}</h3>
-          </li>
-
-          <li>
-            <a
-              className={styles.link}
-              href="/perfil"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.profileName}
-            </a>
-          </li>
-          <li>
-            <a
-              className={styles.link}
-              href="/seguridad"
-              onClick={() => closeMainMenu()}
-            >
-              {titles.security}
-            </a>
-          </li>
-
-          <li>
-            <button className={styles.Logout} onClick={logout}>
-              {titles.logout}
-            </button>
-          </li>
-        </ul>
-        <div
-          className={
-            menuStatus === "opened"
-              ? styles.menuBackground
-              : styles.menuBackgroundHided
-          }
+      <li>
+        <a
+          className={styles.link}
+          href="/productos"
           onClick={() => closeMainMenu()}
-        ></div>
-      </div>
-    </Router>
+        >
+          {titles.products}
+        </a>
+      </li>
+      <li>
+        <a
+          className={styles.link}
+          href="/ventas"
+          onClick={() => closeMainMenu()}
+        >
+          {titles.salesList}
+        </a>
+      </li>
+      <li>
+        <a
+          className={styles.link}
+          href="/devoluciones-de-clientes"
+          onClick={() => closeMainMenu()}
+        >
+          {titles.buyersReturns}
+        </a>
+      </li>
+      <li>
+        <a
+          className={styles.link}
+          href="/mensajes-de-clientes"
+          onClick={() => closeMainMenu()}
+        >
+          {titles.buyersMessages}
+        </a>
+      </li>
+
+      <li>
+        <h3>{titles.purchases}</h3>
+      </li>
+
+      <li>
+        <a
+          className={styles.link}
+          href="/compras"
+          onClick={() => closeMainMenu()}
+        >
+          {titles.purchasesList}
+        </a>
+      </li>
+      <li>
+        <a
+          className={styles.link}
+          href="/devoluciones"
+          onClick={() => closeMainMenu()}
+        >
+          {titles.returns}
+        </a>
+      </li>
+      <li>
+        <a
+          className={styles.link}
+          href="/mensajes-de-vendedores"
+          onClick={() => closeMainMenu()}
+        >
+          {titles.sellersMessages}
+        </a>
+      </li>
+      <li>
+        <h3>{titles.profile}</h3>
+      </li>
+
+      <li>
+        <a
+          className={styles.link}
+          href="/perfil"
+          onClick={() => closeMainMenu()}
+        >
+          {titles.profileName}
+        </a>
+      </li>
+      <li>
+        <a
+          className={styles.link}
+          href="/seguridad"
+          onClick={() => closeMainMenu()}
+        >
+          {titles.security}
+        </a>
+      </li>
+
+      <li>
+        <button className={styles.Logout} onClick={logout}>
+          {titles.logout}
+        </button>
+      </li>
+    </>
   );
 };
 
 const LoggedOutMenu = () => {
   const { loginWithPopup } = useAuth0();
-  const dispatch = useDispatch();
-  const body = document.getElementById("body");
-  const currentUrl = window.location.pathname;
-  const closeMainMenu = () => {
-    dispatch(setMainMenuToClose());
-    if (
-      currentUrl !== "/mensajes-de-vendedores" &&
-      currentUrl !== "/mensajes-de-clientes"
-    )
-      body.style.overflow = "visible";
-  };
+
   const menuStatus = useSelector((state) => state.MainMenu.status);
   console.log(menuStatus);
   return (
-    <Router>
-      <div
-        className={
-          menuStatus === "opened"
-            ? styles.menuContainer
-            : styles.menuContainerHided
-        }
-      >
-        <ul
-          className={
-            menuStatus === "opened" ? styles.menuCanvas : styles.menuCanvasHided
-          }
-        >
-          <button className={styles.LogInAndOut} onClick={loginWithPopup}>
-            {titles.login}
-          </button>
+    <>
+      <button className={styles.LogInAndOut} onClick={loginWithPopup}>
+        {titles.login}
+      </button>
 
-          <button onClick={loginWithPopup} className={styles.Dialoge}>
-            {titles.loginDialoge}
-          </button>
-        </ul>
-        <div
-          className={
-            menuStatus === "opened"
-              ? styles.menuBackground
-              : styles.menuBackgroundHided
-          }
-          onClick={() => closeMainMenu()}
-        ></div>
-      </div>
-    </Router>
+      <button onClick={loginWithPopup} className={styles.Dialoge}>
+        {titles.loginDialoge}
+      </button>
+    </>
+  );
+};
+
+const LoadingLoginMenu = () => {
+  return (
+    <>
+      <p className={styles.Dialoge}>Cargando...</p>
+    </>
   );
 };
